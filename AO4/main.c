e5dff6cb66f6e15acf0de395003b804d10a930ff
omattman <matnhansen@gmail.com>
2018-10-16 13:19:19 +0200

    final solution to the task assignment

--- AO4/main.c
+++ AO4/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+// Define symbolic constants
+#define SENTINEL 0
+
 // Prototypes
 void solveQuadraticEquation(double, double, double);
 double solveDiscriminant(double, double, double);
 double solveRootOne(double, double, double);
 double solveRootTwo(double, double, double);
 
+// Main function and logic to handle input integers.
 int main(void) {
   // Declare variables
-  double a, b, c;
+  double a = 0,
+         b = 0,
+         c = 0;
 
-  // Prompt user for three integers
+  // Prompt user for three integers and prompt again after equation until all integers is equal to 0, then EXIT.
   // Assign input to variables
-  printf("Enter coefficients a, b, and c: ");
-  scanf("%lf %lf %lf", &a, &b, &c);
+  do {
+    printf("Enter coefficients a, b, and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-  // Call function to see if 0, 1 or 2 roots.
-  solveQuadraticEquation(a, b, c);
+    if (a != SENTINEL || b != SENTINEL || c != SENTINEL)
+      solveQuadraticEquation(a, b, c);
+  } while (a != SENTINEL || b != SENTINEL || c != SENTINEL);
 
   return 0;
 }
@@ -27,9 +35,9 @@ void solveQuadraticEquation(double a, double b, double c){
   if (solveDiscriminant(a, b, c) < 0) {
     printf("No roots was found\n");
   } else if (solveDiscriminant(a, b, c) == 0) {
-    printf("One root was found:\nx1 = %.2f", solveRootOne(a, b, c));
+    printf("One root was found:\nx1 = %.2f\n", solveRootOne(a, b, c));
   } else {
-    printf("Two roots was found:\nx1 = %.2f and x2 = %.2f", solveRootOne(a, b, c), solveRootTwo(a, b, c));
+    printf("Two roots was found:\nx1 = %.2f and x2 = %.2f\n", solveRootOne(a, b, c), solveRootTwo(a, b, c));
   }
 }
 
